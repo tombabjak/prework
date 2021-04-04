220bed9a92d50b083f664049a38513ec04019c1c
Tom Babjak <thomas.babjak@gmail.com>
2021-04-03 20:32:51 -0400

    files added

---
+++ Desktop/BootcampRutgers/whileloop_practice.py
@@ -0,0 +1,11 @@
+user_play == "y"
+
+while user_play == "y"
+    user_number = int(input("How many numbers?"))
+
+    for x in range(user_number):
+
+        print(x)
+
+    user_play = input(Continue: (y)es or (n)o?")
+
